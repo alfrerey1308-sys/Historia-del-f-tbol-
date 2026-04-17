0594eaca02e2ec04fc068b440d1ae142950c5648
alfrerey1308-sys <alfrerey1308@gmail.com>
2026-04-16 20:08:11 -0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+console.log("Historia del Fútbol cargada ⚽");
+
+// Efecto para resaltar el menú
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav a');
+
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 150 && rect.bottom >= 150) {
+            const id = section.getAttribute('id');
+            navLinks.forEach(link => {
+                link.classList.remove('activo');
+                if(link.getAttribute('href') === '#' + id) {
+                    link.classList.add('activo');
+                }
+            });
+        }
+    });
+});
